b43f64d979ebd288baacec4d8ba89af8fa5d0331
Pol MB <pol.monclus@hispatec.com>
2024-05-03 14:54:25 +0200

    feat: creada nueva app pokemon

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>¿Qué Pokémon es?</h1>
+
+    <PokemonPicture />
+    <PokemonOptions />
+
+    <!-- TODO opciones -->
+  </div>
+</template>
+
+<script>
+import PokemonOptions from '../components/PokemonOptions.vue'
+import PokemonPicture from '../components/PokemonPicture.vue'
+
+export default {
+    components: {
+        PokemonOptions,
+        PokemonPicture
+    }
+}
+</script>
