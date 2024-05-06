83da85856aed4745c80ea8591e7a7ebe1ef65209
Zeppelin17 <polmonclus@outlook.com>
2024-05-06 16:52:20 +0200

    feat: hasta clase 94

--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -2,21 +2,34 @@
   <div>
     <h1>¿Qué Pokémon es?</h1>
 
-    <PokemonPicture />
-    <PokemonOptions />
+    <PokemonPicture :pokemonId="6" :showPokemon="true" />
+    <PokemonOptions :pokemons="pokemonArr" />
 
-    <!-- TODO opciones -->
   </div>
 </template>
 
 <script>
 import PokemonOptions from '../components/PokemonOptions.vue'
 import PokemonPicture from '../components/PokemonPicture.vue'
+import getPokemonOptions from '../helpers/getPokemonOptions.js'
 
 export default {
     components: {
         PokemonOptions,
         PokemonPicture
-    }
+    },
+    data() {
+      return {
+        pokemonArr: [],
+      }
+    },
+    methods: {
+      async mixPokemonArray() {
+        this.pokemonArr = await getPokemonOptions()
+      },
+    },
+    mounted() {
+      this.mixPokemonArray()
+    },
 }
 </script>
