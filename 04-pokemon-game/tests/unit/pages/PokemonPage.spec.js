585f483d2fd858aa4dc1623d03bd8169f18a94c4
Zeppelin17 <polmonclus@outlook.com>
2024-05-10 20:20:49 +0200

    feat: sección 8 terminada

--- 04-pokemon-game/tests/unit/pages/PokemonPage.spec.js
+++ 04-pokemon-game/tests/unit/pages/PokemonPage.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, mount } from "@vue/test-utils"
 import PokemonPage from "@/pages/PokemonPage.vue"
+import { pokemons } from "../mocks/pokemons.mock"
 
 
 describe("PokemonPage component", () => {
@@ -13,5 +14,63 @@ describe("PokemonPage component", () => {
         const mixPokemonArraySpy = jest.spyOn(PokemonPage.methods, "mixPokemonArray")
         const wrapper = shallowMount(PokemonPage)
         expect(mixPokemonArraySpy).toHaveBeenCalled()
+    })
+
+    test("match con snapshot cuando cargan pokemons", () => {
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ""
+                }
+            }
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    test("se muestran los componentes PokemonPicture y PokemonOptions", () => {
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ""
+                }
+            }
+        })
+        const picture = wrapper.find("pokemon-picture-stub")
+        const options = wrapper.find("pokemon-options-stub")
+        expect(picture.exists()).toBeTruthy()
+        expect(options.exists()).toBeTruthy()
+        expect(picture.attributes("pokemonid")).toBe("1")
+        expect(options.attributes("pokemons")).toBeTruthy()
+    })
+
+    test("pruebas con checkAnswer", async () => {
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ""
+                }
+            }
+        })
+
+        await wrapper.vm.checkAnswer(1)
+        expect(wrapper.find("h2").exists()).toBeTruthy()
+        expect(wrapper.vm.showPokemon).toBeTruthy()
+        expect(wrapper.find("h2").text()).toBe(`Correcto! ${pokemons[0].name}`)
+
+        await wrapper.vm.checkAnswer(10)
+        expect(wrapper.vm.message).toBe(`Oops, era ${pokemons[0].name}`)
     })
 })
