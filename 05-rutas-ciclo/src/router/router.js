e9ec4ffcd166a88e9b5fb905f216f5f019835e44
Zeppelin17 <polmonclus@outlook.com>
2024-05-11 16:55:29 +0200

    feat: seccion 9 hasta 122

---
+++ 05-rutas-ciclo/src/router/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+
+const routes = [
+    { 
+        path: '/', 
+        component: () => import(/*webpackChunkName: "ListPage"*/ '@/modules/pokemon/pages/ListPage.vue'), 
+    },
+    { 
+        path: '/about', 
+        component: () => import(/*webpackChunkName: "AboutPage"*/ '@/modules/pokemon/pages/AboutPage.vue') 
+    },
+    { 
+        path: '/id', 
+        component: () => import(/*webpackChunkName: "PokemonPage"*/ '@/modules/pokemon/pages/PokemonPage.vue'), 
+    },
+    { 
+        path: '/:oathMatch(.*)*', 
+        component: () => import(/*webpackChunkName: "NoPageFound"*/ '@/modules/shared/pages/NoPageFound.vue'), 
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
