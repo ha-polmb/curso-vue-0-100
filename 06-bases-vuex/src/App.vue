028c8ef8d600b0f5bd829ba7cc6bdb0d4dfd15ff
Pol MB <pol.monclus@hispatec.com>
2024-05-16 15:26:25 +0200

    feat: seccion 10 hasta 144

---
+++ 06-bases-vuex/src/App.vue
@@ -0,0 +1,27 @@
+import { defineAsyncComponent } from 'vue';
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <CounterComponent />
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    CounterComponent: defineAsyncComponent(() => import('./components/CounterComponent.vue'))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
