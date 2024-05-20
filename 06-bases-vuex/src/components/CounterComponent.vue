d411b7713bb2283db7a2bf255195e3ff29d13e81
Pol MB <pol.monclus@hispatec.com>
2024-05-20 17:24:15 +0200

    feat: seccion 10 hasta 149

--- 06-bases-vuex/src/components/CounterComponent.vue
+++ 06-bases-vuex/src/components/CounterComponent.vue
@@ -3,19 +3,35 @@
         <h1>Counter -Vuex</h1>
         <h2>Direct access: {{ $store.state.count }}</h2>
         <h2>Computed: {{ countComputed }}</h2>
+        
+        <button @click="increment">+1</button>
+        <button @click="incrementBy">+5</button>
+        <button @click="incrementRandomInt" :disabled="isLoading">+random</button>
+
         <h2>MapState: {{ count }}</h2>
+        <h2>Last mutation: {{ lastMutation }}</h2>
+        <h2>Direct getter: {{ $store.getters.squareCount }}</h2>
     </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
     computed: {
         countComputed() {
             return this.$store.state.count
         },
-        ...mapState(['count'])
+        ...mapState(['count', 'lastMutation', 'isLoading'])
+    },
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+        incrementBy() {
+            this.$store.commit('incrementBy', 5)
+        },
+        ...mapActions(['incrementRandomInt'])
     }
 }
 </script>
