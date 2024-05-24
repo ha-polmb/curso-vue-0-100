af38315b4e55435c7661f3dd9ec802e0ebdffc8d
Zeppelin17 <polmonclus@outlook.com>
2024-05-24 06:53:45 +0200

    feat: seccion 11 has 150

---
+++ 06-projects-app/src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col">
+        <!-- top menu -->
+        <top-menu />
+        <main>
+            <!-- side menu -->
+            <side-menu />
+
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
--- 06-projects-app/src/router/index.ts
+++ 06-projects-app/src/router/index.ts
@@ -1,9 +1,14 @@
+import ProjectsLayout from '@/modules/projects/layouts/ProjectsLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout
+    }
   ]
 })
 
