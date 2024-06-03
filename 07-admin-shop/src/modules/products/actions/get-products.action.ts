dbaf76569c53ddbb32764c4602f400351c8d5026
Pol MB <pol.monclus@hispatec.com>
2024-06-03 10:01:31 +0200

    feat: seccion 14 hasta 199

--- 07-admin-shop/src/modules/products/actions/get-products.action.ts
+++ 07-admin-shop/src/modules/products/actions/get-products.action.ts
@@ -1,13 +1,17 @@
 import { tesloApi } from '@/api/tesloApi';
 import type { Product } from '../interfaces/product.interface';
+import { getProductImageAction } from './get-product-image.action';
 
 export const getProductsAction = async (page: number = 1, limit: number = 10) => {
   try {
     const { data } = await tesloApi.get<Product[]>(
       `/products/?limit=${limit}&offset=${limit * page}`,
     );
-    console.log(data);
-    return data;
+
+    return data.map((product) => ({
+      ...product,
+      images: product.images.map(getProductImageAction),
+    }));
   } catch (error) {
     throw new Error(`Error getting productos: ${error}`);
   }
