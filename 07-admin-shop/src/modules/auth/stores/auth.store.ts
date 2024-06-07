d31452d7dd2f7c1a20bce27ec3776dc1b8825ca5
Pol MB <pol.monclus@hispatec.com>
2024-06-07 10:06:24 +0200

    feat: Seccion 15 hasta 215

--- 07-admin-shop/src/modules/auth/stores/auth.store.ts
+++ 07-admin-shop/src/modules/auth/stores/auth.store.ts
@@ -1,7 +1,7 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { AuthStatus, type User } from '../interfaces';
-import { loginAction, registerAction } from '../actions';
+import { checkAuthAction, loginAction, registerAction } from '../actions';
 import { useLocalStorage } from '@vueuse/core';
 
 export const useAuthStore = defineStore('auth', () => {
@@ -53,6 +53,24 @@ export const useAuthStore = defineStore('auth', () => {
     return false;
   };
 
+  const checkAuthStatus = async (): Promise<boolean> => {
+    try {
+      const statusResp = await checkAuthAction();
+      if (!statusResp.ok) {
+        logout();
+        return false;
+      }
+
+      authStatus.value = AuthStatus.Authenticated;
+      user.value = statusResp.user;
+      token.value = statusResp.token;
+      return true;
+    } catch (error) {
+      logout();
+      return false;
+    }
+  };
+
   return {
     user,
     token,
@@ -67,5 +85,6 @@ export const useAuthStore = defineStore('auth', () => {
     // Actions
     login,
     register,
+    checkAuthStatus,
   };
 });
