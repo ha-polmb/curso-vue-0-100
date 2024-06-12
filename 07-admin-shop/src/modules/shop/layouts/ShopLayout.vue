03c18b053a9a6658b0f239593135317eb179777f
Pol MB <pol.monclus@hispatec.com>
2024-06-12 09:19:42 +0200

    feat: seccion 19 hasta 265

--- 07-admin-shop/src/modules/shop/layouts/ShopLayout.vue
+++ 07-admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -1,16 +1,12 @@
 <template>
-  <!-
-  components: { CustomFooter },
-  components: { CustomFooter },- Header Navbar -->
   <TopMenu />
+  <router-view />
 
- <router-view />
-
-  <!-- Footer --> 
+  <!-- Footer -->
   <custom-footer />
 </template>
 
 <script setup lang="ts">
-import CustomFooter from "../components/CustomFooter.vue";
-import TopMenu from "../components/TopMenu.vue";
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
 </script>
