59ba61e662e1d4f2a53dd424f617efd8eaef4b1f
Pol MB <pol.monclus@hispatec.com>
2024-06-19 09:25:04 +0200

    seccion 20 hasta 295

--- 09-quasar/src/composables/useUI.js
+++ 09-quasar/src/composables/useUI.js
@@ -1,15 +1,20 @@
 import { useStore } from "vuex";
 import { computed } from "vue";
 
-
 export const useUI = () => {
+  const store = useStore();
 
-    const store = useStore()
-
-    return {
-
-        // side menu options
-        sideMenuOpen: computed(() => store.getters["ui/isSideMenuOpen"]),
-        toggleSideMenu: () => store.commit("ui/toggleSideMenu")
-    }
-}
+  return {
+    // side menu options
+    // sideMenuOpen: computed(() => store.getters["ui/isSideMenuOpen"]),
+    sideMenuOpen: computed({
+      get() {
+        return store.getters["ui/isSideMenuOpen"];
+      },
+      set(val) {
+        store.commit("ui/toggleSideMenu");
+      },
+    }),
+    toggleSideMenu: () => store.commit("ui/toggleSideMenu"),
+  };
+};
--- 09-quasar/src/pages/FlexPage.vue
+++ 09-quasar/src/pages/FlexPage.vue
@@ -1,6 +1,34 @@
 <template>
-  <q-page class="flex">
-    <h1>FlexPage.vue</h1>
+  <q-page>
+    <div class="row q-mb-md">
+      <div class="col bg-primary">1</div>
+      <div class="col bg-secondary">2</div>
+      <div class="col bg-accent">3</div>
+    </div>
+    <div class="row q-mb-md">
+      <div class="col bg-primary">1</div>
+      <div class="col col-5 bg-secondary">2</div>
+      <div class="col col-6 bg-accent">3</div>
+    </div>
+    <div class="row q-mb-md justify-center">
+      <div class="col col-4 bg-primary">1</div>
+      <div class="col col-4 bg-secondary">2</div>
+    </div>
+    <div class="row q-mb-md justify-end">
+      <div class="col col-4 bg-primary">1</div>
+      <div class="col col-4 bg-secondary">2</div>
+    </div>
+    <div class="row q-mb-md justify-between">
+      <div class="col col-4 bg-primary">1</div>
+      <div class="col col-4 bg-secondary">2</div>
+    </div>
+
+    <q-separator spaced />
+
+    <div class="row q-mb-md justify-between">
+      <div class="col-12 col-md-4 col-sm-5 bg-primary">1</div>
+      <div class="col-12 col-md-4 col-sm-5 bg-secondary">2</div>
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +37,12 @@ defineOptions({
   name: "FlexPage",
 });
 </script>
+
+<style scoped>
+.col,
+.col-4,
+.col-6,
+.col-12 {
+  height: 50px;
+}
+</style>
