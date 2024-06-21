7bbff79732f0ef6a3e5bdb73e3a3918433332c81
Zeppelin17 <polmonclus@outlook.com>
2024-06-21 22:39:17 +0200

    feat: seccion 21 hasta 315

--- maps-app/src/components/mapview/MapView.ts
+++ maps-app/src/components/mapview/MapView.ts
@@ -1,14 +1,38 @@
-import { usePlacesStore } from "@/composables";
-import { defineComponent } from "vue";
+import { usePlacesStore } from "@/composables"
+import Mapboxgl from "mapbox-gl"
+import { defineComponent, onMounted, ref, watch } from "vue"
 
 export default defineComponent({
   name: "MapView",
   setup() {
-    const { isLoading, userLocation, isUserLocationReady } = usePlacesStore();
+    const mapElement = ref<HTMLDivElement>()
+    const { userLocation, isUserLocationReady } = usePlacesStore()
+
+    const initMap = async () => {
+      if (!mapElement.value) throw new Error("Div Element no exsts")
+      if (!userLocation.value) throw new Error("User location no exists")
+
+      await Promise.resolve()
+
+      const map = new Mapboxgl.Map({
+        container: mapElement.value, // container ID
+        style: "mapbox://styles/mapbox/streets-v12", // style URL
+        center: userLocation.value, // starting position [lng, lat]
+        zoom: 15, // starting zoom
+      })
+    }
+
+    onMounted(() => {
+      if (isUserLocationReady.value) return initMap()
+    })
+
+    watch(isUserLocationReady, (newVal) => {
+      if (isUserLocationReady.value) initMap()
+    })
+
     return {
-      isLoading,
-      userLocation,
       isUserLocationReady,
-    };
+      mapElement,
+    }
   },
-});
+})
--- maps-app/src/main.ts
+++ maps-app/src/main.ts
@@ -1,11 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+
+import mapboxgl from "mapbox-gl" // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiemVwcGVsaW4xNyIsImEiOiJjbHhwNDllNWEwbHg5MmtzZTViZmk4eHFoIn0.AVD9tDH9BJwHcN3p9PiDDA"
 
 if (!navigator.geolocation) {
-  alert("Tu navegador no soporta geolocation");
-  throw new Error("Tu navegador no soporta geolocation");
+  alert("Tu navegador no soporta geolocation")
+  throw new Error("Tu navegador no soporta geolocation")
 }
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).mount("#app")
