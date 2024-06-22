43d78a6a4fc8f83ee96bb0851b7a6016253dd621
Zeppelin17 <polmonclus@outlook.com>
2024-06-22 16:48:56 +0200

    feat: seccion 21 hasta 318

--- maps-app/src/components/mapview/MapView.ts
+++ maps-app/src/components/mapview/MapView.ts
@@ -1,4 +1,4 @@
-import { usePlacesStore } from "@/composables"
+import { useMapStore, usePlacesStore } from "@/composables"
 import Mapboxgl from "mapbox-gl"
 import { defineComponent, onMounted, ref, watch } from "vue"
 
@@ -7,6 +7,7 @@ export default defineComponent({
   setup() {
     const mapElement = ref<HTMLDivElement>()
     const { userLocation, isUserLocationReady } = usePlacesStore()
+    const { setMap } = useMapStore()
 
     const initMap = async () => {
       if (!mapElement.value) throw new Error("Div Element no exsts")
@@ -16,10 +17,25 @@ export default defineComponent({
 
       const map = new Mapboxgl.Map({
         container: mapElement.value, // container ID
-        style: "mapbox://styles/mapbox/streets-v12", // style URL
+        style: "mapbox://styles/mapbox/light-v10", // style URL
         center: userLocation.value, // starting position [lng, lat]
         zoom: 15, // starting zoom
       })
+
+      const myLocationPopup = new Mapboxgl.Popup().setLngLat(userLocation.value)
+        .setHTML(`
+            <h4>Aqui estoy</h4
+            <p>Estoy en este lugar</p>
+            <p>${userLocation.value}</p>
+        `)
+
+      const myLocationMarker = new Mapboxgl.Marker()
+        .setLngLat(userLocation.value)
+        .setPopup(myLocationPopup)
+        .addTo(map)
+
+      // establecer el mapa en Vuex
+      setMap(map)
     }
 
     onMounted(() => {
--- maps-app/src/store/index.ts
+++ maps-app/src/store/index.ts
@@ -1,19 +1,24 @@
-import { createStore } from "vuex";
+import { createStore } from "vuex"
 
 // My custom modules
-import placesModule from "./places";
-import { PlacesState } from "./places/state";
+import placesModule from "./places"
+import { PlacesState } from "./places/state"
+
+import mapModule from "./map"
+import { MapState } from "./map/state"
 
 export interface StateInterface {
   // Define your own store structure, using submodules if needed
   // example: ExampleStateInterface;
   // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
   // example: ExampleStateInterface
-  places: PlacesState;
+  places: PlacesState
+  map: MapState
 }
 
 export default createStore<StateInterface>({
   modules: {
     places: placesModule,
+    map: mapModule,
   },
-});
+})
