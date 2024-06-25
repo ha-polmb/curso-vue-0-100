c009f1a2014c85fb590e497fafcefd85dd4242ca
Pol MB <pol.monclus@hispatec.com>
2024-06-25 16:58:28 +0200

    feat: seccion 21 hasta 322

--- maps-app/src/composables/useMapStore.ts
+++ maps-app/src/composables/useMapStore.ts
@@ -1,19 +1,22 @@
-import { StateInterface } from "@/store"
-import Mapboxgl from "mapbox-gl"
-import { computed } from "vue"
-import { useStore } from "vuex"
+import { StateInterface } from "@/store";
+import Mapboxgl from "mapbox-gl";
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 export const useMapStore = () => {
-  const store = useStore<StateInterface>()
+  const store = useStore<StateInterface>();
 
   return {
     map: computed(() => store.state.map.map),
     distance: computed(() => store.state.map.distance),
     duration: computed(() => store.state.map.duration),
 
+    // Getters
+    isMapReady: computed<boolean>(() => store.getters["map/isMapReady"]),
+
     // Mutations
     setMap: (map: Mapboxgl.Map) => store.commit("map/setMap", map),
 
     // Actions
-  }
-}
+  };
+};
